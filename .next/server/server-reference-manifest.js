aa1bbff6f0ed5cf0dd40a5f6ade79ccf69bc3cb6
luceva2002 <luceva2002@gmail.com>
2025-10-10 01:28:23 +0200

    sempre vercel

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"z6N11QMB/zd3VBK07saFtRsvOizvV+4oRDDQasjpK5Q=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"ibC6FWt4D0k4T3QEdQHkPBcaLqeo+e/9SZITuxWt9f8=\"}"
